76e648c7809e7cb6768e58d585741d031b1b8d98
MS-SBLLBSJHOOAL\Administrator <867999030@qq.com>
2020-10-23 16:40:34 +0800

    123

--- src/study/dataType/array_slice_map.go
+++ src/study/dataType/array_slice_map.go
@@ -3,14 +3,15 @@ package main
 import "fmt"
 
 func main() {
-	var int_arr =[9]int{1,2,8,1,8,9}  //让编译器根据实际的元素个数自行推断数组的长度 [1 2 8 1 8 9 0 0 0]
-	var hobby = [2]string{} //数组
-	var personnel = make([]string, 8)  //切片，长度为8
+	//var int_arr1 =[...]int{1,2,8,1,8,9}  //让编译器根据实际的元素个数自行推断得出数组的长度 [1 2 8 1 8 9 0 0 0]
+	var int_arr = [9]int{1, 2, 8, 1, 8, 9} //让编译器根据实际的元素个数自行推断得出数组的长度 [1 2 8 1 8 9 0 0 0]
+	var hobby = [2]string{}                //数组
+	var personnel = make([]string, 8)      //切片，长度为8
 	hobby[0] = "basketball"
 	hobby[1] = "football"
 
-	c := map[string]int{"Hello":1, "World":2}  //map类型
-	for k, v := range c{
+	c := map[string]int{"Hello": 1, "World": 2} //map类型
+	for k, v := range c {
 		fmt.Println(k, v)
 	}
 	fmt.Println(len(c))
@@ -21,7 +22,7 @@ func main() {
 	fmt.Println(int_arr)
 	fmt.Println(hobby)
 	fmt.Println(personnel)
-	for i, n := range hobby{
+	for i, n := range hobby {
 		fmt.Println(i, n)
 	}
 }
