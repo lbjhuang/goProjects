30293a64b076e75b7b64ba9e60b4f57604a1a025
MS-SBLLBSJHOOAL\Administrator <867999030@qq.com>
2020-10-23 17:51:14 +0800

    Update point.go

--- src/study/dataType/point.go
+++ src/study/dataType/point.go
@@ -3,13 +3,14 @@ package main
 import "fmt"
 
 func main() {
-	var c *int   //声明一个整型指针c
+	var c *int   //声明一个整型指针c，此时内存地址是nil， *c = 18  的写法是不对的，空指针没地址
 
+	fmt.Println(c)  //打印c -> nil
 
 	a := 123
 	d := 108
 	b :=&a   //类型推导直接声明一个指针b
-	fmt.Println(b)
+	fmt.Println(b)  //打印a的内存地址
 	m := *b
 	fmt.Println(m)   //123
 
