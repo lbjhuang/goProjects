653a2266b61e7e8986a38e8edda622a337f0f354
lbjhuang <867999030@qq.com>
2020-10-26 22:29:46 +0800

    struce

---
+++ src/study/dataType/interface1.go
@@ -0,0 +1,46 @@
+package main
+
+import "fmt"
+
+//定义一个接口，里面提供一个方法，约束后来者实现这个方法，接口只有方法没有方法体，不能有值
+type Speaker interface {
+	speak()
+}
+
+type Cat struct {
+	name string
+}
+
+type Dog struct {
+	name string
+}
+
+type human struct {
+	name string
+}
+
+func (c Cat) speak() {        //三个结构体都实现了接口的speak方法
+	fmt.Println(c.name,"is 喵喵")
+}
+
+func (d Dog) speak() {
+	fmt.Println(d.name,"is 旺旺")
+}
+func (h human) speak() {
+	fmt.Println(h.name,"is 哦哦")
+}
+
+func da(x Speaker)  {
+	x.speak()
+}
+
+func main() {
+	var c1 = Cat{name:"lucky"}
+	var d1 = Dog{name:"lamb"}
+	var h1 = human{name:"jiagu"}
+
+	da(c1)
+	da(d1)
+	da(h1)
+}
+
---
+++ src/study/dataType/struct2.go
@@ -0,0 +1,59 @@
+package main
+
+import "fmt"
+
+//指针类型接受者和值类型接受者
+type User struct {
+	name string
+	email string
+}
+
+//1. 当接受者为指针类型时，可以通过该方法改变接受者成员变量的值，即使使用了非指针类型实例调用该函数，也可以改变实例对应的成员变量的值。
+//func (u *User) changeName() {  //指针类型接受者
+//	u.name = "james"
+//}
+//
+//func main() {
+//	u := &User{"perter","1258158@qq.com"}           //创建指针类型结构体实例
+//	fmt.Println("name: ",u.name,"email: ",u.email)
+//	u.changeName()
+//	fmt.Println("name: ",u.name,"email: ",u.email)
+
+
+//输出：
+//name:  perter email:  1258158@qq.com
+//name:  james email:  1258158@qq.com
+
+
+
+//func (u *User) changeName() {  //指针类型接受者
+//	u.name = "james"
+//}
+//
+//func main() {
+//	u := User{"perter","1258158@qq.com"}  //创建非指针类型结构体实例
+//	fmt.Println("name: ",u.name,"email: ",u.email)
+//	u.changeName()
+//	fmt.Println("name: ",u.name,"email: ",u.email)
+//
+//	//同样输出：
+//	//name:  perter email:  1258158@qq.com
+//	//name:  james email:  1258158@qq.com
+//}
+
+
+//值类型接受者：当接受者不是一个指针的时候，传过来的u是对应接受者值的副本，无法改变原来u的成员变量的值
+func (u User) changeName() {  //值类型接受者，里面的方法操作内容将直接影响到接受者
+	u.name = "james"
+}
+
+func main() {
+	u := &User{"perter","1258158@qq.com"}  //创建指针类型结构体实例
+	fmt.Println("name: ",u.name,"email: ",u.email)
+	u.changeName()
+	fmt.Println("name: ",u.name,"email: ",u.email)
+
+	//输出：
+	//name:  perter email:  1258158@qq.com
+	//name:  perter email:  1258158@qq.com
+}
