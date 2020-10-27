bfe2152a0bf777c1468839428498a4661e5266da
MS-SBLLBSJHOOAL\Administrator <867999030@qq.com>
2020-10-27 18:48:12 +0800

    Update main.go

--- src/library_pro/main.go
+++ src/library_pro/main.go
@@ -5,10 +5,12 @@ import "github.com/gin-gonic/gin"
 func main() {
 	r := gin.Default()
 	r.GET("/ping", func(c *gin.Context) {
-		c.JSON(200, gin.H{
-			"message": "pingpangball",
-		})
+		data := map[string]string{}
+		//c.JSON(200, data)
+		//data["name"] = c.Query("name") //获取参数值
+		data["name"] = c.DefaultQuery("name","wade") //获取参数值，没有则指定默认值wade
+		c.JSON(200, data)              //返回json
 	})
-	r.Run() // listen and serve on 0.0.0.0:8080
+	r.Run(":9000") // listen and serve on 0.0.0.0:8080
 
 }
