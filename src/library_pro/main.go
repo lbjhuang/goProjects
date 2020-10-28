fa42b9f8e991c7e0b729e3921dcf193cb2d80c5c
MS-SBLLBSJHOOAL\Administrator <867999030@qq.com>
2020-10-28 18:33:03 +0800

    重新设置代码结构
    重新设置代码结构

--- src/library_pro/main.go
+++ src/library_pro/main.go
@@ -15,7 +15,7 @@ type UserInfo struct {
 	User     string `form:"user" json:"user" xml:"user"  binding:"required"`            //User  结构体首字母一定要大些
 	Password string `form:"password" json:"password" xml:"password" binding:"required"` //Password  结构体首字母一定要大些
 }
-
+var my_db *sql.DB
 type ERPPerson struct {
 	Id         int
 	ChineseName string
@@ -70,7 +70,10 @@ func main() {
 			person  ERPPerson
 			persons []ERPPerson
 		)
-		rows, _ := db.Query("select id,chineseName,englishName,postName from erp_employeeInfo where staffState < ? order by id desc ;",3)
+		rows, err := db.Query("select id,chineseName,englishName,postName from erp_employeeInfo where staffState < ? order by id desc ;",3)
+		if err != nil {
+			fmt.Print("mysql 报错了：",err.Error())
+		}
 
 		for rows.Next() {
 			err = rows.Scan(&person.Id, &person.ChineseName, &person.EnglishName, &person.PostName)
@@ -91,6 +94,38 @@ func main() {
 			"count":  len(persons),
 		})
 	})
+
+	r.GET("/persons", func(c *gin.Context) {
+		var (
+			person  ERPPerson
+			persons []ERPPerson
+		)
+		rows, err := db.Query("select id,chineseName,englishName,postName from erp_employeeInfo where staffState < ? order by id desc ;",3)
+		if err != nil {
+			fmt.Print("mysql 报错了：",err.Error())
+		}
+
+		for rows.Next() {
+			err = rows.Scan(&person.Id, &person.ChineseName, &person.EnglishName, &person.PostName)
+			persons = append(persons, person)
+			if err != nil {
+				fmt.Print(err.Error())
+			}
+		}
+		var is_ok = map[int]string{1611:"nice",1006:"nice"}
+		//循环里面进行赋值
+		for key, val := range persons {
+			persons[key].IsON = is_ok[val.Id]
+		}
+
+		defer rows.Close()
+		c.JSON(http.StatusOK, gin.H{
+			"result": persons,
+			"count":  len(persons),
+		})
+	})
+
+
 
 
 
