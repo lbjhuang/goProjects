5e5e88b7b22e3163fd8d532628a8fed47d2583b2
lbjhuang <dddawang@163.com>
2022-03-05 16:38:21 +0800

    可变参数

--- src/study/func/func2.go
+++ src/study/func/func2.go
@@ -15,9 +15,24 @@ func sumandsem1(a, b int) (sum int, sem int) {
 	return
 }
 
+//可变参数，会用数组接收
+func sumandsem5(b ... int) (int)  {
+	sum := 0
+	//遍历数组计算相加
+	for _, value := range b {
+		sum += value
+	}
+	return sum
+}
+
 func main() {
 	a := 5
 	b := 2
-	c, d := sumandsem1(a, b)
-	fmt.Println("main", c, d)
+	//多返回值 下划线忽略结果
+	//c,d := sumandsem(a, b)
+	c,_ := sumandsem1(a, b)
+	fmt.Println("main", c)
+
+	g:= sumandsem5(a, b,5,6)
+	fmt.Println("main", g)
 }
