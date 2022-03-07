7c620fc1ddec2a2a1a31918700544d76bdbde75f
lbjhuang <dddawang@163.com>
2022-03-07 14:49:40 +0800

    结构体继承

---
+++ src/study/dataType/struct4.go
@@ -0,0 +1,25 @@
+package main
+
+import "fmt"
+
+//结构体继承
+type OnePerson struct {
+	id   int
+	name string
+	age  int
+}
+
+//student 以属性的方式继承OnePerson
+type Student struct {
+	OnePerson
+	id        int
+	className string
+	score     float32
+}
+
+func main() {
+	var s = Student{
+		OnePerson{1, "james", 22}, 2, "class1", 98.5}
+
+	fmt.Printf("one student named %s, his score is %f, he is from %s and he is just %d", s.name, s.score, s.className, s.age)
+}
